94c7ff900bc09bc0b8c2066c3040dddbe9c84609
cale <caleanderson9@gmail.com>
2020-04-08 21:05:52 -0500

    add show, create, update and destroy actions

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,42 @@
 class Api::UsersController < ApplicationController
+  def index
+    @users = User.all
+    render "index.json.jb"
+  end
+
+  
+  def create
+    @user =User.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password_digest: params[:password_digest],
+      total_miles: params[:total_miles]
+    )
+    @user.save
+    render 'show.json.jb'
+  end
+  
+  def show
+    @user = User.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.update(
+      first_name: params[:first_name] || @user.first_name,
+      last_name: params[:last_name] || @user.last_name,
+      email: params[:email] || @user.email,
+      total_miles: params[:total_miles] || @user.total_miles
+    )
+    @user.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy
+    render json: {message: "user has been deleted"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/users" => "users#index"
+    post "/users" => "users#create"
+    get "/users/:id" => "users#show"
+    patch "/users/:id" => "users#update"
+    delete "/users/:id" => "users#destroy"
+  end
 end
